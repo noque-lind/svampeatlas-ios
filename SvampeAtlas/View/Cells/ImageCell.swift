9c077d23362d614019b02a726e030c153c5f4010
Emil Lind <emilsen@Emils-MacBook-Pro.local>
2018-03-06 13:31:00 +0100

    DetailsVC initial setup, images with spring header

---
+++ SvampeAtlas/View/Cells/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCell.swift
+//  SvampeAtlas
+//
+//  Created by Emil Lind on 06/03/2018.
+//  Copyright © 2018 NaturhistoriskMuseum. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    
+}
---
+++ SvampeAtlas/View/MushroomDetailsScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  MushroomDetailsScrollView.swift
+//  SvampeAtlas
+//
+//  Created by Emil Lind on 06/03/2018.
+//  Copyright © 2018 NaturhistoriskMuseum. All rights reserved.
+//
+
+import UIKit
+
+class MushroomDetailsScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

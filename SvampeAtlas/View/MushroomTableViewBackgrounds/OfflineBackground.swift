a5261c46bf2049d7434e9df1692b56f5aa072487
Emil Lind <emilsen@Emils-MacBook-Pro.local>
2018-03-18 15:37:52 +0100

    Unknown changes

---
+++ SvampeAtlas/View/MushroomTableViewBackgrounds/OfflineBackground.swift
@@ -0,0 +1,21 @@
+//
+//  OfflineBackground.swift
+//  SvampeAtlas
+//
+//  Created by Emil Lind on 13/03/2018.
+//  Copyright © 2018 NaturhistoriskMuseum. All rights reserved.
+//
+
+import UIKit
+
+class OfflineBackground: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
